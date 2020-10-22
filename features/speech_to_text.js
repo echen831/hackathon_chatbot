1d22aa815a2768e21b6b3bd933c5d8e55f0c7957
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-10-21 17:34:36 -0700

    Complete onSpeechend function

--- features/speech_to_text.js
+++ features/speech_to_text.js
@@ -3,4 +3,13 @@ function runSpeechRecognition() {
   var action = document.getElementById("action");
   var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
+
+  recognition.onstart = function () {
+    action.innerHTML = "<small>listening, please speak...</small>";
+  }
+  
+  recognition.onspeechend = function () {
+    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+    recognition.stop();
+  }
 }
